fdb8d690db47b545d1c88e7ea1c1841c768daf90
Jolinaroberson <Jolina.roberson@gmail.com>
2021-11-10 19:12:52 -0500

    First commit

---
+++ Week7Day3Practice.js
@@ -0,0 +1,21 @@
+// function myDisplayer(sum) {
+//     document.getElementById("demo").innerHTML = some;
+//   }
+  
+//   function myCalculator(num1, num2) {
+//     let sum = num1 + num2;
+//     return sum;
+//   }
+  
+//   let result = myCalculator(5, 5);
+//   myDisplayer(result);
+
+// setInterval(myFunction, 1000);
+
+// function myFunction() {
+//   let d = new Date();
+//   document.getElementById("demo").innerHTML=
+//   d.getHours() + ":" +
+//   d.getMinutes() + ":" +
+//   d.getSeconds();
+// }
